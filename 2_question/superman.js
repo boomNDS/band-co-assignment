3743894ecd3fa0100a682254c2ee2ba0c3edb9ad
boomNDS <boomsixboom@gmail.com>
2024-06-14 15:01:13 +0700

    feat: we save flock of chickens 🐔

--- 2_question/superman.js
+++ 2_question/superman.js
@@ -1,21 +1,36 @@
+export function getInfo(rawData) {
+  const [info, rawChickenPositions] = rawData.trim().split("\n");
+  const supermanHelpLength = +info.split(" ")[1].trim() ?? 0;
+  const totalChicken = +info.split(" ")[0] ?? 0;
+  const chickenPositions =
+    rawChickenPositions
+      .trim()
+      .split(" ")
+      .slice(0, totalChicken)
+      .map((i) => +i) ?? [];
+  return { supermanHelpLength, chickenPositions, totalChicken };
+}
+
 /**
  * Problem 2: Superman's Chicken Rescue
+ *  [p,p+k) = p≤x<p+k.
  * @param {String} rawData
  * @return {String} Bad boy or Good boy
  */
-
-export function getInfo(rawData) {
-  console.log("test");
-  const [info, rawChickenPosition] = rawData.trim().split("\n");
-  const supermanHelpLength = +info.split(" ")[1] ?? 0;
-  const chickenPosition = rawChickenPosition.split(" ").map((i) => +i) ?? [];
-  return { supermanHelpLength, chickenPosition };
-}
 export function supermanRescue(rawData) {
-  const { supermanHelpLength, chickenPosition } = getInfo(rawData);
-  console.log("supermanHelpLength :", supermanHelpLength);
-  console.log("chickenPosition :", chickenPosition);
-}
+  const { supermanHelpLength, chickenPositions, totalChicken } =
+    getInfo(rawData);
+  let rescueAmount = 1;
+  chickenPositions.slice(0, totalChicken - 1).forEach((startPosition) => {
+    const totalRescue = chickenPositions.filter(
+      (chickenPosition) =>
+        chickenPosition >= startPosition &&
+        chickenPosition < startPosition + supermanHelpLength,
+    ).length;
+    if (totalRescue > rescueAmount) {
+      rescueAmount = totalRescue;
+    }
+  });
 
-supermanRescue(`5 5
-2 5 10 12 15`);
+  return rescueAmount;
+}
